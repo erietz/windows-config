d63922115756d33473aeda807519b0505500726d
Ethan Rietz <erietz@werner.com>
2022-07-28 17:21:43 -0500

    use a powershell function, array, and hashtable

--- install_windows.ps1
+++ install_windows.ps1
@@ -1,6 +1,31 @@
-New-Item -Force -ItemType SymbolicLink -Path "$HOME/AppData/Local/nvim" -Target "$HOME/.dotfiles/unix/.config/nvim"
+function Symlink([string]$ActualFile, [string]$SymLink) {
+	New-Item -Force -ItemType SymbolicLink -Path $symLink -Target $ActualFile
+}
 
-New-Item -Force -ItemType SymbolicLink -Path "$HOME/.vsvimrc" -Target "$HOME/git/windows-config/vsvimrc"
-New-Item -Force -ItemType SymbolicLink -Path "$HOME/.bashrc" -Target "$HOME/git/windows-config/bashrc"
-New-Item -Force -ItemType SymbolicLink -Path "$HOME/.bash_profile" -Target "$HOME/git/windows-config/bash_profile"
-New-Item -Force -ItemType SymbolicLink -Path "$HOME/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json" -Target "$HOME/git/windows-config/settings.json"
+$pairs = @(
+	@{
+		'ActualFile' = "$HOME/.dotfiles/unix/.config/nvim"
+		'SymLink' = "$HOME/AppData/Local/nvim"
+	},
+	@{
+		'ActualFile' ="$HOME/git/windows-config/vsvimrc"
+		'SymLink' = "$HOME/.vsvimrc"
+	},
+	@{
+		'ActualFile' = "$HOME/git/windows-config/bashrc"
+		'SymLink' = "$HOME/.bashrc"
+	},
+	@{
+		'ActualFile' = "$HOME/git/windows-config/bash_profile"
+		'SymLink' = "$HOME/.bash_profile"
+	},
+	@{
+		'ActualFile' = "$HOME/git/windows-config/settings.json"
+		'SymLink' = "$HOME/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
+	}
+)
+
+Foreach ($pair in $pairs)
+{
+	Symlink @pair
+}
